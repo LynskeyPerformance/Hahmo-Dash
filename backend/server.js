385c0864ccf7a40eed5dfe519961d1ee5ef2594d
Baileigh Brown <baileighbrown@lynskeyDev.local>
2022-11-23 08:04:37 -0500

    adding dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,9 @@
-var express = require('express');
+const express = require('express');
+const http = requuire('http');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 var app = express();
 
-const PORT = process.env.API_PORT|| 8080
+const PORT = process.env.API_PORT|| 8080    
