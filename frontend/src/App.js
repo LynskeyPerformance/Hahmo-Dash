227460c45f4310fdc8980976f798d40a64029655
Baileigh Brown <baileighbrown@lynskeyDev.local>
2022-12-22 15:18:58 -0500

    creating functionalities for light and dark theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react"; 
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+
+//this function is for theme, light and dark modes thats stored in the state. Material UI documentation? 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(()=> createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
-   
+   <ThemeProvider theme={theme}>
+     <CssBaseline /> {/*its like a css reset*/}
+   </ThemeProvider>
     </div>
   );
 }
--- frontend/src/state/index.js
+++ frontend/src/state/index.js
@@ -1,5 +1,5 @@
+//creating function that allows us to change the mode from dark to light 
 import { createSlice } from "@reduxjs/toolkit"; 
-
 const initialState = {
     mode: "dark"
 };
@@ -7,9 +7,13 @@ const initialState = {
 export const globalSlice = createSlice ({
     name: "global",
     initialState, 
-    reducers:{
+    reducers: {
+        //function that changes the 'global (everywhere)' state 
         setMode: (state) => {
-            
+             state.mode = state.mode === 'light' ? 'dark' : 'light'; 
         }
     }
 })
+
+export const { setMode } = globalSlice.actions; 
+export default globalSlice.reducer; 
