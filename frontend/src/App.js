d72e4bea7bd43214ef6f043524be6d4ae1000d06
Baileigh Brown <baileighbrown@lynskeyDev.local>
2022-12-27 10:42:25 -0500

    left side search bar is found

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
+//import { Dashboard } from "@mui/icons-material";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react"; 
 import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
+import Layout from "scenes/layout";
+import Dashboard from "scenes/dashboard";
 
 //this function is for theme, light and dark modes thats stored in the state. Material UI documentation? 
 function App() {
@@ -11,9 +15,17 @@ function App() {
 
   return (
     <div className="app">
-   <ThemeProvider theme={theme}>
-     <CssBaseline /> {/*its like a css reset*/}
-   </ThemeProvider>
+  <BrowserRouter> 
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> {/*its like a css reset*/}
+      <Routes> 
+        <Route element={<Layout />}> {/* every route thats in here will have the same sidebar and navbar layout */}
+        <Route path="/" element={<Navigate to="/dashboard" replace />} /> {/* if we go to the default homepage we will be navigated to the dashboard  */}
+        <Route path="/dashboard" element={<Dashboard />} /> {/* this is the dashboard route which will be rendered */}
+        </Route>
+      </Routes>
+    </ThemeProvider>
+  </BrowserRouter>
     </div>
   );
 }
